3a8f2b3a34f1f95a15c6f8a574814e8780463861
Graeme Mathieson <mathie@woss.name>
2013-05-29 17:39:56 +0100

    And a remarkably similar plugin for tags.
    TODO: Refactor common behaviour...

---
+++ _plugins/tags.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  class TagPage < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'archive.html')
+      self.data['tag'] = tag
+      self.data['posts'] = site.tags[tag]
+
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Tag: '
+      self.data['title'] = "#{tag_title_prefix}#{tag}"
+    end
+  end
+
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'archive'
+        dir = site.config['tag_dir'] || 'tags'
+        site.tags.keys.each do |tag|
+          site.pages << TagPage.new(site, site.source, File.join(dir, tag.downcase.gsub(/[^0-9a-z]+/, '-')), tag)
+        end
+      end
+    end
+  end
+end
